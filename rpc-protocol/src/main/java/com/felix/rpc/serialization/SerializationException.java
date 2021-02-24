fc73449183093caabc7faf371a5891b390d0c847
Felix <423126323@qq.com>
2021-02-24 11:47:22 +0800

    init: SerializationException

---
+++ rpc-protocol/src/main/java/com/felix/rpc/serialization/SerializationException.java
@@ -0,0 +1,26 @@
+package com.felix.rpc.serialization;
+
+/**
+ * @description: 序列化异常类
+ * @author: Felix
+ * @date: 2021/2/24 11:46
+ */
+public class SerializationException extends RuntimeException {
+    private static final long serialVersionUID = 3365624081242234230L;
+
+    public SerializationException() {
+        super();
+    }
+
+    public SerializationException(String msg) {
+        super(msg);
+    }
+
+    public SerializationException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public SerializationException(Throwable cause) {
+        super(cause);
+    }
+}
