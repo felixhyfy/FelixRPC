ea11cfdea46a6cc5582926aab4d816b361c7fc10
Felix <423126323@qq.com>
2021-02-24 21:42:37 +0800

    init: RegistryFactory 注册中心工厂类

---
+++ rpc-registry/src/main/java/com/felix/rpc/provider/registry/RegistryType.java
@@ -0,0 +1,11 @@
+package com.felix.rpc.provider.registry;
+
+/**
+ * @description: 注册中心枚举
+ * @author: Felix
+ * @date: 2021/2/24 21:31
+ */
+public enum RegistryType {
+    ZOOKEEPER,
+    EUREKA;
+}
