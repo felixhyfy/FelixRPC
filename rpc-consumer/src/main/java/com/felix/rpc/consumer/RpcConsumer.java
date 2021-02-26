36e85b5c5c822fdf31b275c3b6af8205fab3d5e3
Felix <423126323@qq.com>
2021-02-26 10:52:46 +0800

    bugfix: 解决timeoutException问题

--- rpc-consumer/src/main/java/com/felix/rpc/consumer/RpcConsumer.java
+++ rpc-consumer/src/main/java/com/felix/rpc/consumer/RpcConsumer.java
@@ -85,45 +85,4 @@ public class RpcConsumer {
         }
 
     }
-
-    /*private final Bootstrap bootstrap;
-    private final EventLoopGroup eventLoopGroup;
-
-    public RpcConsumer() {
-        bootstrap = new Bootstrap();
-        eventLoopGroup = new NioEventLoopGroup(4);
-        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline()
-                                .addLast(new FelixRpcEncoder())
-                                .addLast(new FelixRpcDecoder())
-                                .addLast(new RpcResponseHandler());
-                    }
-                });
-    }
-
-    public void sendRequest(FelixRpcProtocol<FelixRpcRequest> protocol, RegistryService registryService) throws Exception {
-        FelixRpcRequest request = protocol.getBody();
-        Object[] params = request.getParams();
-        String serviceKey = RpcServiceHelper.buildServiceKey(request.getClassName(), request.getServiceVersion());
-
-        int invokerHashCode = params.length > 0 ? params[0].hashCode() : serviceKey.hashCode();
-        ServiceMeta serviceMetadata = registryService.discovery(serviceKey, invokerHashCode);
-
-        if (serviceMetadata != null) {
-            ChannelFuture future = bootstrap.connect(serviceMetadata.getServiceAddr(), serviceMetadata.getServicePort()).sync();
-            future.addListener((ChannelFutureListener) arg0 -> {
-                if (future.isSuccess()) {
-                    log.info("connect rpc server {} on port {} success.", serviceMetadata.getServiceAddr(), serviceMetadata.getServicePort());
-                } else {
-                    log.error("connect rpc server {} on port {} failed.", serviceMetadata.getServiceAddr(), serviceMetadata.getServicePort());
-                    future.cause().printStackTrace();
-                    eventLoopGroup.shutdownGracefully();
-                }
-            });
-            future.channel().writeAndFlush(protocol);
-        }
-    }*/
 }
